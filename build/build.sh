11173281c948d3202380ac7d81bba3c600347ec9
kaplarn <kaplarn@kaplarn-N501VW>
2017-08-01 15:00:26 +0200

    Fix build

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 rm -Rf ./src
+rm -Rf ./schema.sql
+
 mkdir src
 rsync -av ../src/ ./src/
+rsync -av ../schema.sql ./schema.sql
 
 docker build --no-cache --pull -t kaplarn/salesautopilot .
 #docker push kaplarn/salesautopilot
 
 rm -Rf ./src
+rm -Rf ./schema.sql
